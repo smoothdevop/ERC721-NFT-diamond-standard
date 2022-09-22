b46e1e028188532442039da37ff42469446c4463
Jesserc <raymondjesse713@gmail.com>
2022-09-22 14:09:04 +0100

    first commit

---
+++ contracts/libraries/AppStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+struct AppStorage {
+    // Token name
+    string _name;
+    // Token symbol
+    string _symbol;
+    // Owner address for checks
+    address owner;
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) _owners;
+    // Mapping owner address to token count
+    mapping(address => uint256) _balances;
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) _tokenApprovals;
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) _operatorApprovals;
+}
+
+library LibAppStorage {
+    function diamondStorage() internal pure returns (AppStorage storage ds) {
+        assembly {
+            ds.slot := 0
+        }
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,89 @@
+/* global ethers hre */
+/* eslint prefer-const: "off" */
+
+const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
+
+async function deployDiamond () {
+  const accounts = await ethers.getSigners()
+  const contractOwner = accounts[0];
+  // const contractOwner = "0x571B102323C3b8B8Afb30619Ac1d36d85359fb84"
+
+  // deploy DiamondCutFacet
+  const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
+  const diamondCutFacet = await DiamondCutFacet.deploy()
+  await diamondCutFacet.deployed()
+  console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
+
+  // deploy Diamond
+  const Diamond = await ethers.getContractFactory('Diamond')
+  const diamond = await Diamond.deploy(contractOwner.address, diamondCutFacet.address)
+  await diamond.deployed()
+  console.log('Diamond deployed:', diamond.address)
+
+  // deploy DiamondInit
+  const DiamondInit = await ethers.getContractFactory('DiamondInit')
+  const diamondInit = await DiamondInit.deploy()
+  await diamondInit.deployed()
+  console.log('DiamondInit deployed:', diamondInit.address)
+
+  // deploy facets
+  console.log('')
+  console.log('Deploying facets')
+  const FacetNames = [
+    'DiamondLoupeFacet',
+    'OwnershipFacet',
+    'DiamondNFT'   
+  ]
+  const cut = []
+  for (const FacetName of FacetNames) {
+    const Facet = await ethers.getContractFactory(FacetName)
+    const facet = await Facet.deploy()
+    await facet.deployed()
+    console.log(`${FacetName} deployed: ${facet.address}`)
+    cut.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: getSelectors(facet)
+    })
+  }
+
+  // upgrade diamond with facets
+  console.log('')
+ try {
+  console.log('Diamond Cut:', cut)
+  const diamondCut = await ethers.getContractAt('IDiamondCut', diamond.address)
+  let tx
+  let receipt
+  // call to init function
+  console.log("============================================================================");
+  console.log("=============================calling init function==========================");
+  let functionCall = diamondInit.interface.encodeFunctionData('init')
+
+  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall)
+
+  console.log("============================================================================");
+  console.log("=============================called init function==========================");
+  console.log('Diamond cut tx: ', tx.hash)
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  }
+  console.log('Completed diamond cut')
+  return diamond.address
+ } catch (error) {
+  console.log("ERROR==========",error);
+ }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  deployDiamond()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.deployDiamond = deployDiamond
